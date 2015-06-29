152eab1175c03bbc004e86ffb0fce264cb786ee4
ux9 <ux9@users.noreply.github.com>
2015-06-29 16:41:31 -0700

    simple game

---
+++ guessgame.rb
@@ -0,0 +1,15 @@
+
+puts "Guess Game "
+puts "enter number between 0 and 100"
+n = gets.chomp
+r = rand(100)
+a = 0
+while n != r
+
+ puts "Try Again !"
+  n = gets.chomp 
+  a += 1
+ 
+  break if n == r  
+end    
+puts "You guess the number in #{a} attempts"
